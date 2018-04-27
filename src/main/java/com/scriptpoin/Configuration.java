18cad23bfc8d59904a1b4f7c73d48121edeccc6f
William França <williamucep@hotmail.com>
2018-04-27 13:51:24 -0300

    Pontapé inicial do ServerSide Gestação Saudável

---
+++ src/main/java/com/scriptpoin/Configuration.java
@@ -0,0 +1,16 @@
+package com.scriptpoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Configuration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Configuration.class, args);
+    }
+
+}
